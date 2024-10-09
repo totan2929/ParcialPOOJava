f8e9ec28396baf035541dc448f9aabe56247ba75
Jonathan Cañola Salazar <jocasa90@hotmail.com>
2024-10-09 17:50:02 -0500

    created Class Bookind with methods

--- src/main/java/org/ciaf/booking/Booking.java
+++ src/main/java/org/ciaf/booking/Booking.java
@@ -4,27 +4,64 @@ import org.ciaf.customer.Customer;
 import org.ciaf.vehicle.Vehicle;
 
 import java.time.LocalDate;
+import java.time.Period;
+import java.util.Scanner;
 
 public class Booking {
     private int idBooking;
     private Customer customer;
     private Vehicle vehicle;
     private LocalDate startDate;
-    private LocalDate enddate;
+    private LocalDate endDate;
     private double totalCost;
 
-    public Booking(int idBooking, Customer customer, Vehicle vehicle, LocalDate startDate, LocalDate enddate, double totalCost) {
+    public Booking(int idBooking, Customer customer, Vehicle vehicle, LocalDate startDate, LocalDate endDate, double totalCost) {
         this.idBooking = idBooking;
         this.customer = customer;
         this.vehicle = vehicle;
         this.startDate = startDate;
-        this.enddate = enddate;
+        this.endDate = endDate;
         this.totalCost = totalCost;
     }
 
     public void confirmReservation(Vehicle vehicle){
+        Period totalDays = Period.between(startDate,endDate);
+        int days = totalDays.getDays();
+        int confirm;
+        boolean insurance = false;
+        boolean gps = false;
+        int insurance1;
+        int gps1;
+        Scanner sc = new Scanner(System.in);
         if (vehicle.isAvailability() == true){
-            vehicle.setAvailability(false);
+            System.out.println("Deseas realizar la reserva:\n" +
+                    "Digita\n" +
+                    "1. Si\n" +
+                    "2. No");
+            confirm = sc.nextInt();
+            if (confirm == 1){
+                vehicle.setAvailability(false);
+                System.out.println("Deseas incluir seguro en la reserva:\n" +
+                        "Digita\n" +
+                        "1. Si\n" +
+                        "2. No");
+                insurance1 = sc.nextInt();
+                System.out.println("Deseas incluir GPS en la reserva:\n" +
+                        "Digita\n" +
+                        "1. Si\n" +
+                        "2. No");
+                gps1 = sc.nextInt();
+                if (insurance1 == 1){
+                    insurance = true;
+                }
+                if (gps1 == 1){
+                    gps = true;
+                }
+                totalCost =vehicle.calculatePrice(days, insurance, gps);
+
+                System.out.println("The total cost without insurance and without GPS is: " + totalCost);
+            }
+
         }else {
             System.out.println("We are sorry, the requested vehicle is not available.!!!!!");
         }
@@ -47,7 +84,7 @@ public class Booking {
     }
 
     public LocalDate getEnddate() {
-        return enddate;
+        return endDate;
     }
 
     public double getTotalCost() {
